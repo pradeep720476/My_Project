2bf5e5455925674f5d0c4be21508d1f034ae91bc
anuj-k-s <anujkumarsoni1993@gmail.com>
2019-07-20 19:21:42 +0530

    changes related to manifest and cross origin

--- mypharamacy/src/main/java/com/mypharamacy/controller/CustomerController.java
+++ mypharamacy/src/main/java/com/mypharamacy/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.mypharamacy.entity.User;
 import com.mypharamacy.service.CustomerService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("customer")
 public class CustomerController {
 
