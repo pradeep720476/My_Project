68989276ef981f9f26ae359d88d1f49d03b90049
PradeepKumar <kumarpradeep353@gmail.com>
2019-07-20 12:24:56 +0530

    created project

---
+++ mypharamacy/src/main/java/com/mypharamacy/MypharamacyApplication.java
@@ -0,0 +1,13 @@
+package com.mypharamacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypharamacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypharamacyApplication.class, args);
+	}
+
+}
